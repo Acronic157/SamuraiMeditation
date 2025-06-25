1db9f4d9d0baa648bcf4888e92a8dae917a0ef4e
Acronic157 <65405040+Acronic157@users.noreply.github.com>
2025-06-25 20:36:22 +0200

    TileMaps, Platforms, Slopes,new animations, leveldesign
    I added tilemaps with different usecases, platforms with a script (only the first platform is working), I did some level design as kinda a tutorial level, I added the new animation spritesheet. Slopes are still buggy, When Camera attached to player, it turns black when walking left (Prob because some freeze or so)

---
+++ SamuraiMeditation/Assets/PlayerOneWayPlatform.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneWayPlatform : MonoBehaviour
+{
+
+    private GameObject currentOneWayPlatform;
+    public float waitForSeconds = 1f;
+    [SerializeField] private CapsuleCollider2D playerCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        { 
+            if (currentOneWayPlatform != null)
+            {
+                StartCoroutine(DisableCollision());
+            }
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = collision.gameObject;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = null;
+        }
+    }
+
+    private IEnumerator DisableCollision()
+    {
+        BoxCollider2D platformCollider = currentOneWayPlatform.GetComponent<BoxCollider2D>();
+
+        Physics2D.IgnoreCollision(playerCollider, platformCollider);
+        yield return new WaitForSeconds(waitForSeconds);
+        Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
+    }
+
+
+
+}
