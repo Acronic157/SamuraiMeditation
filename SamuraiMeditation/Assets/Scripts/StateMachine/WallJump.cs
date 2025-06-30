8670ef06a1bfa74e9de58755a15e209e34e66216
Acronic157 <65405040+Acronic157@users.noreply.github.com>
2025-06-30 23:09:18 +0200

    worked on walljump
    fixed the problem with sideways velocity by putting it into the update function. Still major Bugs tho!

--- SamuraiMeditation/Assets/Scripts/StateMachine/WallJump.cs
+++ SamuraiMeditation/Assets/Scripts/StateMachine/WallJump.cs
@@ -7,6 +7,9 @@ public class WallJump : PlayerState
     // Fabian:
     public float wallJumpXForce = 10f;
     public float wallJumpYForce = 12f;
+    public float wallJumpTime = 0.2f;
+    public int wallDirection = 0;
+    //public bool wallJumpEnded = false;
     public WallJump(player _player, PlayerStateMachine _stateMachine, string _aniboolname) : base(_player, _stateMachine, _aniboolname)
     {
 
@@ -16,24 +19,39 @@ public class WallJump : PlayerState
     {
         base.Enter();
 
-        //Fabian
-        int wallDirection = 1;
-        Debug.Log("Is in WallJump State");
-        Player.rb.gravityScale = 4f;
-        Player.rb.velocity = new Vector2(wallDirection * wallJumpXForce, wallJumpYForce);
-        Player.rb.AddForce(new Vector2(wallDirection * wallJumpXForce, wallJumpYForce) * Time.deltaTime);
-        // If wallcheck -> WallSlide State
+        // right Wall
+        if (Player.WallChecking())
+        {
+            wallDirection = 1;
+        }
+        // left Wall
+        else if (Player.WallChecking2())
+        {
+            wallDirection = -1;
+        }
 
     }
 
     public override void Exit()
     {
+        //wallJumpEnded = false;
         base.Exit();
     }
 
     public override void Update()
     {
         base.Update();
+
+        //Debug.Log(wallJumpEnded);
+
+        wallJumpTime -= Time.deltaTime;
+
+        if (wallJumpTime <= 0.0f)
+        {
+            //wallJumpEnded = true;
+            StateMachine.ChangeState(Player.air);
+        }
+
         if (Player.GroundCheck())
         {
             StateMachine.ChangeState(Player.Idlestate);
@@ -41,8 +59,17 @@ public class WallJump : PlayerState
         }
         if(Player.WallChecking()||Player.WallChecking2())
         {
-           // StateMachine.ChangeState(Player.air);
+           StateMachine.ChangeState(Player.air);
         }
+
+        
+        
+        Debug.Log("Is in WallJump State");
+        Player.rb.gravityScale = 4f;
+        Player.rb.velocity = new Vector2(wallDirection * wallJumpXForce, wallJumpYForce);
+        //Player.rb.AddForce(new Vector2(wallDirection * wallJumpXForce, wallJumpYForce) * Time.deltaTime);
+
+        
         
     }
 
