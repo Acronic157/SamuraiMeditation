b365f1b5575711c086a6c0c30a5ddca9d969b902
UchihaItachi1708 <shubhampatil170803@gmail.com>
2025-06-30 15:34:19 +0530

    Animation added

---
+++ SamuraiMeditation/Assets/Scripts/StateMachine/Dead.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dead : PlayerState
+{
+    public Dead(player _player, PlayerStateMachine _stateMachine, string _aniboolname) : base(_player, _stateMachine, _aniboolname)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
